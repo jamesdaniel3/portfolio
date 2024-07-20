070313ade1d84c995e6157e3a430a7ce83966f58
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-07-20 16:50:58 -0400

    created basic structure of files for client

---
+++ portfolio_client/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./routes/Home.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
