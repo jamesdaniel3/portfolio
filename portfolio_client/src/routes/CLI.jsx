f052b3e033fb7d5734d1965d87c8809fc59b7641
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-07-20 18:45:31 -0400

    added intro paragraph to cli page

--- portfolio_client/src/routes/CLI.jsx
+++ portfolio_client/src/routes/CLI.jsx
@@ -2,7 +2,16 @@ export default function CLI() {
   return (
     <>
       <div className="page-content cli-page-content">
-        <p> This is the cli page</p>
+        <p>
+          {" "}
+          I spend a huge portion of my time during the school year working as a
+          teaching assistant for Introduction to Python and Data Structures and
+          Algorithms II. On occasion, there have been tasks that take a large
+          portion of our time as TAs that I thought could be optimized. To solve
+          for this, I have created a couple of scripts that run from the command
+          line to improve the experience of our TAs and Students. They can be
+          found below.{" "}
+        </p>
       </div>
     </>
   );
