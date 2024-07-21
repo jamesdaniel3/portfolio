4f51a4501a42e42b9cd2b4af3579a24b656d32c4
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-07-20 22:58:14 -0400

    edited home page css

--- portfolio_client/src/routes/Home.jsx
+++ portfolio_client/src/routes/Home.jsx
@@ -8,11 +8,11 @@ export default function Home() {
         <div className="introduction">
           <p className="heading">Hi, I'm James</p>
           <p className="subtext">
-            I'm a developer with expereince in fullstack development. I'm
+            I'm a developer with experience in fullstack development. I'm
             passionate about programming because I love solving problems and the
             practice provides ample opportunity to do so. I built this site to
             display my work. On this page, you will find a couple of my
-            projectss, but feel free to navigate the site to get a larger sample
+            projects, but feel free to navigate the site to get a larger sample
             of my work.
           </p>
         </div>
