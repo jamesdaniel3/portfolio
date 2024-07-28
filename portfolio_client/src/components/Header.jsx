9c57b58734d9502501d94222886eeaa86e1589ee
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-07-28 18:06:17 -0400

    made page header fixed

--- portfolio_client/src/components/Header.jsx
+++ portfolio_client/src/components/Header.jsx
@@ -9,47 +9,57 @@ import menu from "../assets/icons/menu.png";
 
 export function Header() {
   return (
-    <div className={"header"}>
-      <div className={"header-section main-page-links"}>
-        <Link className="navbar-link" to={"/web"}>
-          <span>Web</span>
-        </Link>
-        <Link className="navbar-link" to={"/cli"}>
-          <span>CLI</span>
-        </Link>
-        <Link className="navbar-link" to={"/upcoming"}>
-          <span>In-the-works</span>
-        </Link>
-      </div>
-      <div className={"header-section header-title"}>
-        <Link className="navbar-link" to={"/"}>
-          <span className={"title"}>
-            <strong>James Daniel</strong>
-          </span>
-        </Link>
-      </div>
-      <div className="header-section social-links">
-        <a
-          href="https://github.com/jamesdaniel3"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={github} alt="github link" className="image github-image" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/james-daniel-5b48b01b0/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={linkedIn}
-            alt="linedin link"
-            className="image linkedin-image"
-          />
-        </a>
-        <a href="mailto:jamesmd333@gmail.com" rel="noopener noreferrer">
-          <img src={contact} alt="contact me" className="image contact-image" />
-        </a>
+    <div className="header-container">
+      <div className={"header"}>
+        <div className={"header-section main-page-links"}>
+          <Link className="navbar-link" to={"/web"}>
+            <span>Web</span>
+          </Link>
+          <Link className="navbar-link" to={"/cli"}>
+            <span>CLI</span>
+          </Link>
+          <Link className="navbar-link" to={"/upcoming"}>
+            <span>In-the-works</span>
+          </Link>
+        </div>
+        <div className={"header-section header-title"}>
+          <Link className="navbar-link" to={"/"}>
+            <span className={"title"}>
+              <strong>James Daniel</strong>
+            </span>
+          </Link>
+        </div>
+        <div className="header-section social-links">
+          <a
+            href="https://github.com/jamesdaniel3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={github}
+              alt="github link"
+              className="image github-image"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/james-daniel-5b48b01b0/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={linkedIn}
+              alt="linedin link"
+              className="image linkedin-image"
+            />
+          </a>
+          <a href="mailto:jamesmd333@gmail.com" rel="noopener noreferrer">
+            <img
+              src={contact}
+              alt="contact me"
+              className="image contact-image"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
